605e972f6d4a6bfc59c867f3b794773149686ec6
JP Bustamante <cuchobustamante@gmail.com>
2021-08-24 13:03:14 -0500

    basic git

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, "/client/src/index.jsx");
+const DIST_DIR = path.join(__dirname, "/client/dist");
+
+
+module.exports = {
+  mode: 'development',
+  entry: SRC_DIR,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+    module: {
+      rules: [
+        {
+          test: /.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader'
+        }
+      ]
+    }
+}
